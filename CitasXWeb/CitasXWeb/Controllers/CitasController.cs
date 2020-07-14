dea6ab8128345a4d5ac9887b855d48d379a49ef5
JosafatMurillo <josagears@hotmail.com>
2020-07-14 10:57:23 -0500

    Se agrega un filtrado de horas disponibles

--- CitasXWeb/CitasXWeb/Controllers/CitasController.cs
+++ CitasXWeb/CitasXWeb/Controllers/CitasController.cs
@@ -19,7 +19,7 @@ namespace CitasXWeb.Controllers
         [Route("/General/Cita")]
         [HttpPost]
         [Produces("application/json")]
-        public Boolean Login([FromForm] TbCita cita)
+        public Boolean AgendarCita([FromForm] TbCita cita)
         {
             _context.TbCita.Add(cita);
             var confirmar = _context.SaveChanges();
@@ -31,6 +31,33 @@ namespace CitasXWeb.Controllers
             }
             ViewBag.registroCita = false;
             return false;
+        }
+
+        // GET api/<controller>/5
+        [Route("/General/HorasCita")]
+        [HttpPost]
+        [Produces("application/json")]
+        public String[] HorasDisponibles([FromForm] DateTime fecha)
+        {
+            String[] horas = { "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30" };
+
+            List<TbCita> citas = _context.TbCita.Where(c => c.CitFecha.Equals(fecha)).OrderBy(c => c.CitHora).ToList();
+
+            //Rangos de horario es de 9 am a 3 pm - con una duración de la cita de 30 min
+            for (int i = 0; i < citas.Count(); i++)
+            {
+                var horarioV = citas.ElementAt(i).CitHora;
+                for (int tiempo = 0; tiempo < horas.Count(); tiempo++)
+                {
+                    if(horas[tiempo] == horarioV)
+                    {
+                        horas = horas.Where(val => val != horarioV).ToArray();
+                        break;
+                    }
+                }
+            }
+
+            return horas;
         }
 
         public IActionResult cancelarRegistroCita()
